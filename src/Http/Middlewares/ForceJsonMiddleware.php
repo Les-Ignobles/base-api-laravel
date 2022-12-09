f7f14511aced6e95d438208b3a47876bce53902e
Théo <tochampion38@gmail.com>
2022-12-09 14:44:24 +0100

    Add ForceJson Middleware

--- src/Http/Middlewares/ForceJsonMiddleware.php
+++ src/Http/Middlewares/ForceJsonMiddleware.php
@@ -9,6 +9,14 @@
 namespace Lesignobles\BaseApiLaravel\Http\Middlewares;
 
 
+use Closure;
+use Illuminate\Http\Request;
+
 class ForceJsonMiddleware
 {
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Accept', 'application/json');
+        return $next($request);
+    }
 }
